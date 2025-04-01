337126d2651a325c6378cb1d90d13597a56a04db
WebRohanRay <WebRohanRay@gmail.com>
2025-04-01 19:46:31 +0530

    Day1

---
+++ Day1/selectionSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>;
+using namespace std;
+void selection_sort(int arr[],int n ){
+    for(int i=0;i<n;i++){
+        int min=i;
+        for(int j=i;j<n;j++){
+            if(arr[min] > arr[j]){
+                min=j;
+            }
+        }
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];;
+    for(int i=0;i<n;i++) 
+    cin >> arr[i];
+selection_sort(arr,n);
+for(int i=0;i<n;i++){
+    cout <<arr[i] <<" ";
+}
+    return 0;
+}
